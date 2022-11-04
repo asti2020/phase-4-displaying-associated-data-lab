ad5a6370a8707c586c4609487b29e055b5e2206e
Astika Luintel <astild2020@gmail.com>
2022-11-03 21:14:37 -0400

    commit tree 21de7d8956109c3ca9838d51a2d3fdee279e96d5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+
+    def index
+        item = Item.all
+        render json: item, include: :user, status: :ok, except: [:created_at, :updated_at]
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show
+        items = User.find(params[:id])
+        render json: items, include: :items, status: :ok, except: [:created_at, :updated_at]
+      end
 end
